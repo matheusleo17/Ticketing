6f5852882e2ede88b83a041d6cab1ba58cd0fe83
Matheus Fernandes De Souza Leo <Ms961086@gmail.com>
2026-03-14 20:34:54 -0300

    feature - inserção do cors

--- Ticketing.Api/Program.cs
+++ Ticketing.Api/Program.cs
@@ -8,12 +8,23 @@ builder.Services.AddControllers();
 builder.Services.AddApplication();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5175")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
-
+app.UseCors("FrontendPolicy");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
